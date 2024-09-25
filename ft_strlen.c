6e00de7dff8a5874feb237bc835317e55b539a16
Sergio Izquierdo Alba <seizquie@c1r15s2.42madrid.com>
2024-09-25 18:56:08 +0200

    perfect

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,11 @@
 /*   By: seizquie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 18:16:18 by seizquie          #+#    #+#             */
-/*   Updated: 2024/09/19 18:50:49 by seizquie         ###   ########.fr       */
+/*   Updated: 2024/09/25 18:55:46 by seizquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-typedef unsigned long size_t; // preguntar si hay que añadir librería <stddef.h> o crear alias: typedef unsigned long size_t  //
 
 size_t	ft_strlen(const char *s)
 {
